875d4c1bdf7c1dfd75c846625b98658902301a40
AKL001 <l9udyr@gmail.com>
2024-10-27 16:49:03 +0100

    Norminette/check

---
+++ strfunctions/ft_atoi.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ablabib <ablabib@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/27 16:28:58 by ablabib           #+#    #+#             */
+/*   Updated: 2024/10/27 16:35:16 by ablabib          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *src)
+{
+	size_t	i;
+	size_t	sign_count;
+	size_t	nb;
+	size_t	sign;
+
+	sign_count = 0;
+	nb = 0;
+	sign = 1;
+	i = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign_count++;
+		i++;
+	}
+	if (sign_count % 2 != 0)
+		sign = -1;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = (nb * 10) + (str[i] - '0');
+		i++;
+	}
+	return (nb * sign);
+}
